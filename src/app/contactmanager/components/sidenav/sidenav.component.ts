e99c2a14ec40d179708d2747842bb35d890ab5e8
WojciechMal <101119195+WojciechMal@users.noreply.github.com>
2022-09-08 16:53:02 +0200

    first commit

---
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -0,0 +1,55 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
+const SMALL_WIDTH_BREAKPOINT = 720;
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  public isScreenSmall: boolean;
+
+  users: Observable<User[]>;
+  isDarkTheme: boolean = false;
+  dir: string = 'ltr';
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private UserService: UserService,
+    private router: Router) { }
+
+    @ViewChild(MatSidenav) sidenav: MatSidenav;
+
+    toggleTheme(){
+      this.isDarkTheme = !this.isDarkTheme;
+    }
+    toggleDir(){
+      this.dir = this.dir == 'ltr' ? 'rtl' : 'ltr';
+    }
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      //.observe([Breakpoints.XSmall])
+      .observe([ `(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+      .subscribe((state: BreakpointState) =>{
+        this.isScreenSmall = state.matches;
+      });
+
+      this.users = this.UserService.users;
+      this.UserService.loadAll();
+
+      this.router.events.subscribe(()=>{
+        if (this.isScreenSmall) {
+          this.sidenav.close();
+        }
+      })
+  }
+
+}
